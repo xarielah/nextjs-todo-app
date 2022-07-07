1593a86fba3da9a015be3fa18811063d559c35cb
arielahr <arielah1997@gmail.com>
2022-07-07 14:36:51 +0300

    added env var

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    LOCAL: 'http://localhost:3000',
-  },
+    reactStrictMode: true,
+    env: {
+        LOCAL: 'http://localhost:3000',
+        PROD: 'https://nextjs-todo-app-woad.vercel.app/'
+    }
 };
 
 module.exports = nextConfig;
