11a7b1163403e5029e0462da5889a7488a9b368b
arielahr <arielah1997@gmail.com>
2022-07-07 14:31:36 +0300

    fixing dev-mode bugs

--- components/cardheader.js
+++ components/cardheader.js
@@ -5,46 +5,50 @@ import axios from 'axios';
 import * as actions from '../utils/actions';
 
 const CardHeader = ({ data, delTaskDialog }) => {
-  const updateArchive = async data => {
-    try {
-      await axios.post(`http://localhost:3000/api/actions/update?action=${actions.UPDATE_ARCHIVE}`, { data });
-      window.location.reload(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const url = process.env.PROD ? process.env.PROD : process.env.LOCAL;
+    const updateArchive = async data => {
+        try {
+            await axios.post(`${url}/api/actions/update?action=${actions.UPDATE_ARCHIVE}`, { data });
+            window.location.reload(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  return (
-    <Box mx={3} mt={3}>
-      <Flex align="center" justify={data.pinned ? 'space-between' : 'flex-end'}>
-        {data.pinned && (
-          <HStack>
-            <StarIcon />
-            <Text>Pinned</Text>
-          </HStack>
-        )}
+    return (
+        <Box mx={3} mt={3}>
+            <Flex align='center' justify={data.pinned ? 'space-between' : 'flex-end'}>
+                {data.pinned && (
+                    <HStack>
+                        <StarIcon />
+                        <Text>Pinned</Text>
+                    </HStack>
+                )}
 
-        <Menu>
-          <MenuButton
-            as={Box}
-            p={1}
-            borderRadius={'md'}
-            _hover={{ color: 'purple.200' }}
-            lineHeight={0}
-            bg={'whiteAlpha.200'}
-            cursor={'pointer'}>
-            <ChevronDownIcon />
-          </MenuButton>
-          <MenuList>
-            <MenuItem onClick={() => updateArchive(data)}>{data.completed ? 'Set as incomplete' : 'Set as completed'}</MenuItem>
-            <MenuItem onClick={() => delTaskDialog(data)} color="red.400">
-              Delete Task
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      </Flex>
-    </Box>
-  );
+                <Menu>
+                    <MenuButton
+                        as={Box}
+                        p={1}
+                        borderRadius={'md'}
+                        _hover={{ color: 'purple.200' }}
+                        lineHeight={0}
+                        bg={'whiteAlpha.200'}
+                        cursor={'pointer'}
+                    >
+                        <ChevronDownIcon />
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem onClick={() => updateArchive(data)}>
+                            {data.completed ? 'Set as incomplete' : 'Set as completed'}
+                        </MenuItem>
+                        <MenuItem onClick={() => delTaskDialog(data)} color='red.400'>
+                            Delete Task
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+            </Flex>
+        </Box>
+    );
 };
 
 export default CardHeader;
